bf0e227a74fb22b01c486100d842cef749a51882
Avni <avni.k.balan@gmail.com>
2020-06-28 17:54:48 +0100

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 09:55:34 2020
+
+@author: Avni
+"""
+
+
+for i in range(1, 101):
+    if i%3 == 0:
+        print('fizz')
+    elif i%5 == 0:
+        print('buzz')
+    elif i%3 == 0 and i%5 == 0:
+        print('fizzbuzz')
+    else:
+        print(i)
